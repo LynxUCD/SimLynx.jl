f6b3a9fe39caf8d072260a58f99302b8cae84175
Anthony <Frenchy2199@yahoo.com>
2021-01-27 13:59:38 -0700

    Adding some unit tests for process

--- test/process.jl
+++ test/process.jl
@@ -1,3 +1,27 @@
 @testset "process.jl" begin
-    println("No Tests written for process.jl")
+
+    @process helloWorld() begin
+        return "Goodbye world"
+    end
+
+    @test helloWorld().name == "helloWorld()"
+    # Still need to test out the Task attribute and all the other ones, although some will be null
+
+    @testset "Process Sig Error" begin
+        try
+            eval(:(@process 4 :4))  # first arg is wrong
+        catch e
+            @test typeof(e.error) === ArgumentError
+        end
+
+    end
+
+    # This testset isn't running... why?
+    @testset "Process Body Error" begin
+        try
+            eval(:(@process helloWorld() 4))    # missing begin/end
+        catch
+            @test typeof(e.error) === ArgumentError
+        end
+    end
 end
