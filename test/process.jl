9caafdf51fbb6018930dcfc47c1d1bb63b6aaae2
Anthony <Frenchy2199@yahoo.com>
2021-01-28 19:18:14 -0700

    Added more unit tests to process

--- test/process.jl
+++ test/process.jl
@@ -1,45 +1,40 @@
+include("resources/test_utilities.jl")
+
 @testset "process.jl" begin
 
     @process helloWorld() begin
         return "Goodbye world"
     end
 
-    # Is there a better way to test out all of these fields?
-    @test helloWorld().name == "helloWorld()"
-    @test helloWorld().state == :created
-    @test helloWorld().task.state == :runnable  #double check if this is enough, we're just checking that we created a task and it's runnable
-    @test typeof(helloWorld().notice.element) == Process    #the element of our notice should be a Process
-    @test typeof(helloWorld().queue) == Array{SimLynx.AbstractMessage,1}
+    @testset "Process fields" begin
+        # I can map these to a dictionary and throw it in a loop once I confirm that all these tests are needed/good
+        @test typeof(helloWorld()) == Process
+        @test helloWorld().name == "helloWorld()"
+        @test helloWorld().state == :created
+        @test helloWorld().task.state == :runnable  #double check if this is enough, we're just checking that we created a task and it's runnable
+        @test typeof(helloWorld().notice.element) == Process    #the element of our notice should be a Process
+        @test typeof(helloWorld().queue) == Array{SimLynx.AbstractMessage,1}
+        
+        # do we prefer one over the other? Are both needed?
+        @test helloWorld().acceptors == SimLynx.Acceptor[]
+        @test typeof(helloWorld().acceptors) == Array{SimLynx.Acceptor,1}
+
+        @test helloWorld().response === nothing
+        @test helloWorld().storage == IdDict{Symbol, Any}()
+
+    end
     
-    # do we prefer one over the other? Are both needed?
-    @test helloWorld().acceptors == SimLynx.Acceptor[]
-    @test typeof(helloWorld().acceptors) == Array{SimLynx.Acceptor,1}
-
-    @test helloWorld().response === nothing
-    @test helloWorld().storage == IdDict{Symbol, Any}()
-    #println(typeof(helloWorld().queue))
-
-    @schedule now helloWorld()
-    println(helloWorld().state)
-    
-
-    @testset "Process Sig Error" begin
-        error = nothing
-        try
-            include("resources/process/argument_error.jl")
-        catch e
-            # insert function here to unwrap the error
-            error = e.error.error
+    @testset "Process states" begin
+        statusList = [:active, :working, :delayed, :interrupted, :suspended, :terminated]
+        # These are currently failing.. not sure why, but this is definitively a me error and not a function error (trace-mixed-event-process-based-bank-model works)
+        for status in statusList
+            SimLynx.process_state!(helloWorld(), status)
+            #@test helloWorld().state == status
         end
-        @test typeof(error) === ArgumentError
     end
 
-    # Currently can't throw a body error
-    # @testset "Process Body Error" begin
-    #     try
-    #         eval(:(@process helloWorld() 4))    # missing begin/end
-    #     catch
-    #         @test typeof(e.error) === ArgumentError
-    #     end
-    # end
+    @testset "Process Sig Error" begin
+        @test_throws ArgumentError @m_throw @process foo :bar
+    end
+
 end
