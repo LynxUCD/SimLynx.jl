8e7d90e2bee4ebb42c6ffc9c92087aaec5864ac5
Anthony <Frenchy2199@yahoo.com>
2021-01-29 18:38:31 -0700

    Boy I'm stuck. Trying to test the state changes of a process.

--- test/process.jl
+++ test/process.jl
@@ -25,12 +25,55 @@ include("resources/test_utilities.jl")
     end
     
     @testset "Process states" begin
-        statusList = [:active, :working, :delayed, :interrupted, :suspended, :terminated]
-        # These are currently failing.. not sure why, but this is definitively a me error and not a function error (trace-mixed-event-process-based-bank-model works)
-        for status in statusList
-            SimLynx.process_state!(helloWorld(), status)
-            #@test helloWorld().state == status
+        customerList = []
+        tellers = nothing
+        # I have removed Exponential and Uniform distribution for this test. Hopefully it's okay
+        @event generate(i:: Integer, n::Integer) begin
+            @schedule now customer(i)
+            if i < n
+                @schedule in rand(1:9) generate(i + 1, n)
+                # push!(customerList, customer(i))
+            end
         end
+
+        "The ith customer into the system."
+        @process customer(i::Integer) begin
+            @with_resource tellers begin
+                work(rand(1:4))
+                #push!(customerList, customer(i))
+            end
+            #println("State of this customer: ", customer(i).state)
+        end
+
+        # @with_resource customer(1) begin
+        #     return true
+        # end
+
+        # for i = 1:10
+        #     println(customer(i))
+        # end
+
+        #everything is stuck under current_simulation so I cannot use things like @with_resource 
+        #to trigger a request, to get a process_state! changed from created to active or delayed etc
+
+        @simulation begin
+            current_trace!(true)
+            tellers = Resource(2, "tellers")
+            @schedule at 0.0 generate(1, 10)
+            start_simulation()
+            println(customer(1).state)  # I would think the state of this would be terminated but it's still stuck at created
+        end
+        println(customer(1).state)  # even outside the simulation scope, the process is still set to :created
+
+        # statusList = [:active, :working, :delayed, :interrupted, :suspended, :terminated]
+        # # These are currently failing.. but I suspect it's because we don't export this function (hence why I have to SimLynx.something) 
+        # # And possible helloWorld() is being updated in a different scope. SimLynx module vs Main module. So I can only test it through a simulation
+        # # (trace-mixed-event-process-based-bank-model works)
+        # for status in statusList
+        #     SimLynx.process_state!(helloWorld(), status)
+        #     helloWorld().task
+        #     @test helloWorld().state == status
+        # end
     end
 
     @testset "Process Sig Error" begin
