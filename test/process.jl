a9aebb450cc808c8a57797cb8c6a143badebb68a
Anthony <Frenchy2199@yahoo.com>
2021-02-11 12:37:22 -0700

    Added all tests to check for every state possible in processes

--- test/process.jl
+++ test/process.jl
@@ -25,62 +25,83 @@ include("resources/test_utilities.jl")
     end
     
     @testset "Process states" begin
-        # TODO: 
-           # use a state list instead
-           # use two processes, control and test process
-            # control can look at the test process state and lock the state of the process
-            # and release to check if the test process is set properly to :create, :delayed, etc...
-
         stateList = []
-        proc_list = []
         tellers = nothing
 
         "The ith customer into the system."
         @process control(i::Integer) begin
             @with_resource tellers begin
-                # current process in here is the control process... how to look at testProcess after scheduling?
-                test1 = testProcess(1)
-                test2 = testProcess(2)
-                @schedule in 0.0 test1 # gets scheduled and will be delayed due to control taking the resource
-                @schedule immediate test2   # control(1) should get interrupted
-                work(rand(1:4)) # takes up the only resource
+                add!(proc_list, current_process())
 
+                test1 = testProcess(1)
+                @schedule now test1 # will be delayed due to control taking the resource
+                @schedule now doNothing(1)  # will suspend itself
+                @schedule now testProcess(2)
+                @schedule now rudeProcess(1)
+                work(rand(2:4)) # takes up the only resource and gets set to :working
                 # use the variable that stored the process to access testProcess(1) and push it's delayed state in the state list
                 push!(stateList, current_process().state)   # :active
                 push!(stateList, test1.state)   # delayed
+
+                remove!(proc_list, current_process())
             end
+            wait(8) # wait for other processes to happen
+            push!(stateList, first(proc_list).state)
+            notice = first(proc_list).notice
+            notice.time = current_time() + 1.0
+            resume(first(proc_list), notice)
         end
 
         @process testProcess(i::Integer) begin
+            if current_process().name == "testProcess(1)"
+                push!(stateList, controlProcess.state)  # pushing :working state
+                push!(stateList, current_process()) #push process which will mutate to terminated eventually
+            end
             @with_resource tellers begin
+                add!(proc_list, current_process())
                 work(rand(1:4))
-                push!(proc_list, current_process()) # if I push the state, it will remain as :active and we'll never see it being updated to terminated
+                remove!(proc_list, current_process())
             end
         end
 
-
-        @simulation begin
-            current_trace!(true)
-            tellers = Resource(1, "tellers")
-            @schedule now control(1)
-            start_simulation()
-            println(stateList)
-            println(proc_list)
+        @process rudeProcess(i::Integer) begin
+            wait(4)
+            #@with_resource tellers begin
+            other_proc = first(proc_list)   # <- this needs to be a in working at this point to be able to be interrupted
+            notice = interrupt(other_proc)
+            notice.time = current_time() + 2.0
+            work(2)
+            # make sure to check for interrupted in other_proc before resuming
+            push!(stateList, other_proc.state)
+            resume(other_proc, notice)
+            #end
         end
 
-        @test stateList[1] == :active
-        @test stateList[2] == :delayed
-        @test proc_list[1].state == :terminated
+        @process doNothing(i::Integer) begin
+            @with_resource tellers begin
+                add!(proc_list, current_process())
+                suspend()
+                work(rand(1:4))
+            end
+        end
 
-        # statusList = [:active, :working, :delayed, :interrupted, :suspended, :terminated]
-        # # These are currently failing.. but I suspect it's because we don't export this function (hence why I have to SimLynx.something) 
-        # # And possible helloWorld() is being updated in a different scope. SimLynx module vs Main module. So I can only test it through a simulation
-        # # (trace-mixed-event-process-based-bank-model works)
-        # for status in statusList
-        #     SimLynx.process_state!(helloWorld(), status)
-        #     helloWorld().task
-        #     @test helloWorld().state == status
-        # end
+        @simulation begin
+            #current_trace!(true)
+            tellers = Resource(1, "tellers")
+            global proc_list = Queue{Process}()
+            global controlProcess = @schedule now control(1)
+            start_simulation()
+            #println(stateList)
+        end
+
+        @test testProcess(5).state == :created
+        @test stateList[1] == :working
+        @test stateList[2].state == :terminated
+        @test stateList[3] == :active
+        @test stateList[4] == :delayed
+        @test stateList[5] == :interrupted
+        @test stateList[6] == :suspended
+        
     end
 
     @testset "Process Sig Error" begin
