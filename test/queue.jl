32d4b9ec7634fa4c49a7ebf3b7e35937dd60e8aa
Anthony <Frenchy2199@yahoo.com>
2021-02-27 17:46:35 -0700

    added unit tests for queue

--- test/queue.jl
+++ test/queue.jl
@@ -1,3 +1,23 @@
 @testset "queue.jl" begin
-    println("No Tests written for queue.jl")
+    queue = Queue{Int64}()
+    enqueue!(queue, 2)
+    add!(queue, 3)
+    add!(queue, 2)
+    @test first(queue) == 2
+    remove!(queue, 2)
+    @test length(queue) == 2
+    @test last(queue) == 2
+    dequeue!(queue)
+    @test isempty(queue) == false
+    dequeue!(queue)
+    @test isempty(queue) == true
+
+    lifo = LifoQueue{Int64}()
+
+    enqueue!(lifo, 2)
+    @test lifo.n.value == 1
+    @test lifo.data[1] == 2
+    dequeue!(lifo)
+    @test lifo.n.value == 0
+
 end
